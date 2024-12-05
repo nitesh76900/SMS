ab5c1640eea7f19ce396e00811dd2283dc0c6d72
Nitesh <niteshkumawat76900@gmail.com>
2024-12-05 15:38:50 +0530

    test 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'frontend/dist', // Ensure this matches the location where your server expects the built files
+  },
 })
